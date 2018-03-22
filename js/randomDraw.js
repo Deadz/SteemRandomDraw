960a0d8ec2ff3135158765077ebdf2444fb02092
deadzy <cocofaudel@gmail.com>
2018-03-22 09:32:09 +0100

    V1.0
    - New interface more professional
    - Header with choice of languages: FR / EN / (RU soon)
    - The app retains our previous choices
    - Correction of the display if no winner, the draw is no longer counted
    - Improved display of results
    - The author of the article can no longer participate.
    - Added option to prevent winning more than once
    - Added option to exclude bots (thanks to CleverShovel)
    - Addition of a summary list of winners, if more than one draw
    - Code enhancement.

--- js/randomDraw.js
+++ js/randomDraw.js
@@ -2,10 +2,10 @@
 
 // SET.VIEW
 	$('#wait').hide();
-	$('#participants').hide();
 	$('#result').hide();
 	$('#nobody').hide();
 	$('#err_div').hide();
+	$('#btn_win_list').hide();
 // END SET.VIEW
 
 // SET.VAR
@@ -13,30 +13,34 @@
 	var vote_participant = [];
 	var coms_participant = [];
 	var participants = [];
+	var win_list = [];
 	var number_of_draws = "1";
-	var no_winner = $("#winner").html();
+	var reset = $("#winner").html();
 // END SET.VAR
 
 // SET.VAL.SAVE
 	$("#vote_field").val(localStorage.vote_min);
 	$("#coms_field").val(localStorage.coms_text);
 	if(localStorage.coms_box == "false")
-	$("#coms_box").prop('checked', false);
+		$("#coms_box").prop('checked', false);
 	else
-	$("#coms_box").prop('checked', true);
+		$("#coms_box").prop('checked', true);
 	if(localStorage.vote_box == "false")
-	$("#vote_box").prop('checked', false);
+		$("#vote_box").prop('checked', false);
 	else
-	$("#vote_box").prop('checked', true);
-	if(localStorage.nobots_box == "false")
-	$("#nobots_box").prop('checked', false);
+		$("#vote_box").prop('checked', true);
+	if(localStorage.bots_box == "false")
+		$("#bots_box").prop('checked', false);
 	else
-	$("#nobots_box").prop('checked', true);
+		$("#bots_box").prop('checked', true);
+	if(localStorage.dble_box == "false")
+		$("#dble_box").prop('checked', false);
+	else
+		$("#dble_box").prop('checked', true);
 // END SET.VAL.SAVE
 
 // END SET
-console.log($("#link_form").serialize());
-console.log(localStorage);
+
 // FUNCTION
 
 function getAuthorPermlink(link_full)
@@ -58,13 +62,7 @@ function getAuthorPermlink(link_full)
 			return true;
 		}
 		else
-		{
-			$('#err_title').html("Error");
-			$('#err_body').html("Please, Provide a valid link like https://busy.org/fr/@deadzy/my-contribution");
-			$('#err_div').show();
-			$("#wait").hide();
 			return false;
-		}
 }
 
 async function getInfoFull(author, permlink)
@@ -75,62 +73,55 @@ async function getInfoFull(author, permlink)
 		console.log("getVoteValid");
 		//console.log(err, result);
 
-		var vote_valid = 0; // Votes valid
-
 		if (err == null) 
 		{
 			sessionStorage.setItem("vote_nb", result.length);
 			localStorage.setItem("vote_min", $('#vote_field')[0].value);
 
 			if ($('#vote_field')[0].value !== "")
-			{
 				vote_min = $('#vote_field')[0].value*100;
-			}
 			else
-			{
 				vote_min = 0;
-			}
-			
-			var participants = [];
-			// DELETING BOTS
-			$.ajax({
-				url: "./list_of_bots/bots.json",
-				dataType: 'json',
-				async: false,
-				data: null,
-				success: function(data) 
-				{
-					for (var i = 0; i < sessionStorage.vote_nb; i++)
-					{
-						var flag = true;
-						for (var j = 0; j < data.bots.length; j++)
-						{
-							if (result[i].voter === data.bots[j])
-							{
-								flag = false;
-								break;
-							}
-						}
-						if (flag)
-						{
-							participants.push(result[i]);
-						}
-					}
-				}
-			});
-			// DELETING BOTS END
-			if (participants.length == 0) participants = result;
-			
-			
-			for (var i = 0; i < participants.length; i++)
+
+			for (var i = 0; i < sessionStorage.vote_nb; i++)
 			{
-			    if(participants[i].percent >= vote_min)
+			    if(result[i].percent >= vote_min && result[i].voter != sessionStorage.author)
 			    {
-			       	vote_participant.push(participants[i].voter);
-			    	vote_valid++;
+			    	if(!$("#dble_box").is(":checked")  && $.inArray(result[i].voter, win_list) >= 0)
+			    	{
+			    		// already win
+			    	}
+			    	else
+			    	{
+			    		if($("#bots_box").is(":checked"))
+			    		{
+			    			$.ajax({
+								url: "./blacklist/bots.json",
+								dataType: 'json',
+								async: false,
+								data: null,
+								success: function(data) 
+								{
+									bots_list = data.bots;
+									if ($.inArray(result[i].voter, bots_list) >= 0)
+									{
+										// Bot
+									}
+									else
+									{
+										vote_participant.push(result[i].voter);
+									}
+								}
+							});
+						}
+			    		else
+			    		{
+			    			vote_participant.push(result[i].voter);
+			    		}
+			    	}
 			    }
 			}
-			sessionStorage.setItem("vote_valid", vote_valid);
+			sessionStorage.setItem("vote_valid", vote_participant.length);
 		}
 	});
 
@@ -139,55 +130,52 @@ async function getInfoFull(author, permlink)
 		console.log("getComsValid");
 		//console.log(err, result);
 
-		coms_valid = 0; // Comments valid
-
 		if(err == null)
 		{
 			sessionStorage.setItem("coms_nb", result.length);
 
-			coms_text = $('#coms_field')[0].value;
-			localStorage.setItem("coms_text", coms_text);
+			localStorage.setItem("coms_text", $('#coms_field')[0].value);
 
-			var participants = [];
-			// DELETING BOTS
-			$.ajax({
-				url: "./list_of_bots/bots.json",
-				dataType: 'json',
-				async: false,
-				data: null,
-				success: function(data) 
+			for (var i = 0; i < sessionStorage.coms_nb; i++)
+			{
+				var regex =  new RegExp(localStorage.coms_text, "ig");
+				if(regex.test(result[i].body) == true && result[i].author != sessionStorage.author)
 				{
-					for (var i = 0; i < sessionStorage.coms_nb; i++)
-					{
-						var flag = true;
-						for (var j = 0; j < data.bots.length; j++)
-						{
-							if (result[i].author === data.bots[j])
-							{
-								flag = false;
-								break;
-							}
+					if(!$("#dble_box").is(":checked")  && $.inArray(result[i].author, win_list) >= 0)
+			    	{
+			    		// already win
+			    	}
+			    	else
+			    	{
+			    		if($("#bots_box").is(":checked"))
+			    		{
+			    			$.ajax({
+								url: "./blacklist/bots.json",
+								dataType: 'json',
+								async: false,
+								data: null,
+								success: function(data) 
+								{
+									bots_list = data.bots;
+									if ($.inArray(result[i].voter, bots_list) >= 0)
+									{
+										// Bot
+									}
+									else
+									{
+										coms_participant.push(result[i].author);
+									}
+								}
+							});
 						}
-						if (flag)
-						{
-							participants.push(result[i]);
-						}
+			    		else
+			    		{
+			    			coms_participant.push(result[i].author);
+			    		}
 					}
 				}
-			});
-			// DELETING BOTS END
-			if (participants.length == 0) participants = result;
-			
-			for (var i = 0; i < participants.length; i++)
-			{
-				var regex =  new RegExp(coms_text, "ig");
-				if(regex.test(participants[i].body) == true)
-				{
-					coms_participant.push(participants[i].author);
-				 	coms_valid++;
-				}
 			}
-			sessionStorage.setItem("coms_valid", coms_valid);
+			sessionStorage.setItem("coms_valid", coms_participant.length);
 		}
 	});
 }
@@ -198,47 +186,37 @@ function getRandomDraw()
 
 	if($("#coms_box").is(":checked") == true)
 	{
-		if($("#vote_box").is(":checked") == true)
+		if($("#vote_box").is(":checked") == true) // Vote & Coms
 		{
-			participant_lenght = vote_participant.length;
-			x = 0;
-			for (i = 0; i < participant_lenght; i++)
+			nb = 0;
+			for (i = 0; i < vote_participant.length; i++)
 			{	
 				if($.inArray(vote_participant[i], coms_participant) >= 0)
 				{
+					nb++;
 					participants.push(vote_participant[i]);
-					x++;
-					$("#tab").append("<tr><td>"+x+"</td><td>"+vote_participant[i]+"</td></tr>");
+					$("#tab").append("<tr><td>"+nb+"</td><td>"+vote_participant[i]+"</td></tr>");
 				}
 			}
-			num = randomNumber(participants.length);
 			sessionStorage.setItem("nb_valid", participants.length);
-			sessionStorage.setItem("winner", participants[num]);
+			sessionStorage.setItem("winner", participants[randomNumber(participants.length)]);
 		}
-		else
+		else // Coms
 		{
-			participant_lenght = coms_participant.length;
-			x = 0;
-			for (i = 0; i < participant_lenght; i++)
+			for (i = 0; i < coms_participant.length; i++)
 			{
-				x++;
-				$("#tab").append("<tr><td>"+x+"</td><td>"+coms_participant[i]+"</td></tr>");
+				$("#tab").append("<tr><td>"+(i+1)+"</td><td>"+coms_participant[i]+"</td></tr>");
 			}
-			num = randomNumber(coms_participant.length);
-			sessionStorage.setItem("winner", coms_participant[num]);
+			sessionStorage.setItem("winner", coms_participant[randomNumber(coms_participant.length)]);
 		}
 	}
-	else
+	else // Vote
 	{
-		participant_lenght = vote_participant.length;
-		x = 0;
-		for (i = 0; i < participant_lenght; i++)
+		for (i = 0; i < vote_participant.length; i++)
 		{
-			x++;
-			$("#tab").append("<tr><td>"+x+"</td><td>"+vote_participant[i]+"</td></tr>");
+			$("#tab").append("<tr><td>"+(i+1)+"</td><td>"+vote_participant[i]+"</td></tr>");
 		}
-		num = randomNumber(vote_participant.length);
-		sessionStorage.setItem("winner", vote_participant[num]);
+		sessionStorage.setItem("winner", vote_participant[randomNumber(vote_participant.length)]);
 	}
 	getResult();
 }
@@ -247,131 +225,163 @@ function getResult()
 {
 	console.log("getResult");
 
-	if(sessionStorage.winner == "undefined")
+	if(sessionStorage.winner != "undefined")
 	{ 
-
+		win_list.push(sessionStorage.winner); // Winner list
+		sessionStorage.setItem("num_of_draws", number_of_draws++); // Number of draw
+		$("#winner").html("<h1><img src='images/steem.png'> <a href='"+sessionStorage.link_site+"/@"+sessionStorage.winner+"' target='_blank'>/@"+sessionStorage.winner+"</a></h1><b class='w3-right'><i class='fa fa-certificate'></i> Certified random draw n°"+sessionStorage.num_of_draws+"</b>");
 	}
-	else
+
+	$('#coms_nb').html(sessionStorage.coms_nb);
+	$('#vote_nb').html(sessionStorage.vote_nb);
+
+	if($("#coms_box").is(":checked") == true) // If opt coms
 	{
-		sessionStorage.setItem("num_of_draws", number_of_draws++);
-		$("#winner").html("<h1><a href='https://busy.org/@"+sessionStorage.getItem("winner")+"' target='_blank'>/@"+sessionStorage.getItem("winner")+"</a></h1><b class='w3-right'><i class='fa fa-certificate'></i> Certified random draw n°"+sessionStorage.getItem("num_of_draws")+"</b>");
+		if($("#vote_box").is(":checked") == true) // If otp coms & vote
+		{
+			$('#cond').html($('#vote_tit').html()+" & "+$('#coms_tit').html()); // Show condition
+			$('#part_nb').html(sessionStorage.nb_valid);
+			if (localStorage.vote_min != "")
+			{
+				if (localStorage.coms_text != "")
+					$('#cond_opt').html($('#vote_cond').html()+" <b>"+localStorage.vote_min+"%</b> & "+$('#coms_cond').html()+" <b>\""+localStorage.coms_text+"\"</b>");
+				else
+					$('#cond_opt').html($('#vote_cond').html()+" <b>"+localStorage.vote_min+"%</b>");
+			}
+			else
+				if (localStorage.coms_text != "")
+					$('#cond_opt').html($('#coms_cond').html()+" <b>\""+localStorage.coms_text+"\"</b>");
+				else
+					$('#cond_opt').html("");
+		}
+		else // If coms only
+		{
+			if (localStorage.coms_text != "")
+				$('#cond_opt').html($('#coms_cond').html()+" <b>\""+localStorage.coms_text+"\"</b>");
+			else
+				$('#cond_opt').html("");
+			$('#cond').html($('#coms_tit').html());
+			$('#part_nb').html(sessionStorage.coms_valid);
+		}
+	}
+	else // If vote only
+	{
+		if (localStorage.vote_min != "")
+			$('#cond_opt').html($('#vote_cond').html()+" <b>"+localStorage.vote_min+"%</b>");
+		else
+			$('#cond_opt').html("");
+		$('#cond').html($('#vote_tit').html());
+		$('#part_nb').html(sessionStorage.vote_valid);
 	}
 
-	$('.vote_nb').html(sessionStorage.getItem("vote_nb"));
-	$('.coms_nb').html(sessionStorage.getItem("coms_nb"));
-	$('.vote_nb_valid').html(sessionStorage.getItem("vote_valid"));
-	$('.coms_nb_valid').html(sessionStorage.getItem("coms_valid"));
-	$('.nb_valid').html(sessionStorage.nb_valid);
+	if(sessionStorage.num_of_draws >= 2)
+		$('#btn_win_list').show();
 
 	$("#wait").hide();
-
-	$('#participants').show();
 	$('#result').show();
+
+	sessionStorage.clear();
 }
 
 function randomNumber(max)
 {
-	return Math.floor(Math.random()*(max));
+	return Math.floor(Math.random()*(max+1));
 }
 
 // FUNCTION END
 
 // EVENT
-
-$("#coms_box").change(function()
-{
-	if($("#coms_box").is(":checked") == true)
+	$("#coms_box").change(function()
 	{
-		$("#coms_field").prop('disabled', false);
-	}
-	else
-	{
-		$("#coms_field").prop('disabled', true);
-		$("#coms_field").prop('value', "");
-		if($("#vote_box").is(":checked") == false)
+		if($("#coms_box").is(":checked") == true)
 		{
-			$("#vote_box").prop('checked', true);
-			$("#vote_field").prop('disabled', false);
-		}
-	}
-}).change();
-
-$("#vote_box").change(function()
-{
-	if($("#vote_box").is(":checked") == true)
-	{
-		$("#vote_field").prop('disabled', false);
-	}
-	else
-	{
-		$("#vote_field").prop('disabled', true);
-		$("#vote_field").prop('value', "");
-		if($("#coms_box").is(":checked") == false)
-		{
-			$("#coms_box").prop('checked', true);
 			$("#coms_field").prop('disabled', false);
 		}
-	}
-}).change();
+		else
+		{
+			$("#coms_field").prop('disabled', true);
+			$("#coms_field").prop('value', "");
+			if($("#vote_box").is(":checked") == false)
+			{
+				$("#vote_box").prop('checked', true);
+				$("#vote_field").prop('disabled', false);
+			}
+		}
+	}).change();
 
-$('#link_form').submit(function(event)
-{
-	event.preventDefault();
-	$("#wait").show();
-
-	// RESET
-
-	$("#tab").html("");
-	$("#winner").html(no_winner);
-
-	$('#participants').hide();
-	$('#result').hide();
-
-	vote_participant = [];
-	coms_participant = [];
-	participants = [];
-
-	// END RESET
-
-	// SAVE SET
-	if($("#coms_box").is(":checked") == true)
+	$("#vote_box").change(function()
 	{
-		localStorage.setItem("coms_box", true);
-	}
-	else
-	{
-		localStorage.setItem("coms_box", false);
-	}
-	if($("#vote_box").is(":checked") == true)
-	{
-		localStorage.setItem("vote_box", true);
-	}
-	else
-	{
-		localStorage.setItem("vote_box", false);
-	}
-	if($("#nobots_box").is(":checked") == true)
-	{
-		localStorage.setItem("nobots_box", true);
-	}
-	else
-	{
-		localStorage.setItem("nobots_box", false);
-	}
-	// END SAVE SET
+		if($("#vote_box").is(":checked") == true)
+		{
+			$("#vote_field").prop('disabled', false);
+		}
+		else
+		{
+			$("#vote_field").prop('disabled', true);
+			$("#vote_field").prop('value', "");
+			if($("#coms_box").is(":checked") == false)
+			{
+				$("#coms_box").prop('checked', true);
+				$("#coms_field").prop('disabled', false);
+			}
+		}
+	}).change();
 
-	var link_full = $('#link_field')[0].value;
-
-	if(getAuthorPermlink(link_full) == true)
+	$("#btn_win_list").click(function()
 	{
-		getInfoFull(sessionStorage.getItem("author"), sessionStorage.getItem("permlink"));
-		setTimeout(getRandomDraw, 2000);
-		sessionStorage.clear();
-	}
-	else
-	{
-		$("#wait").hide();
-	}
-});
+		$("#win_tab").html(""); // RESET
+		for (i = 0; i < win_list.length; i++)
+		{
+			$("#win_tab").append("<tr><td>"+(i+1)+"</td><td>"+win_list[i]+"</td></tr>");
+		}
+		$("#win_view").show();
+	});
 
-		// EVENT END
+	$('#link_form').submit(function(event)
+	{
+		event.preventDefault();
+		$("#wait").show();
+
+		// RESET
+			$("#tab").html("");
+			$("#winner").html(reset);
+			$('#participants').hide();
+			$('#result').hide();
+			vote_participant = [];
+			coms_participant = [];
+			participants = [];
+		// END RESET
+
+		// SAVE SET
+			if($("#coms_box").is(":checked") == true)
+				localStorage.setItem("coms_box", true);
+			else
+				localStorage.setItem("coms_box", false);
+			if($("#vote_box").is(":checked") == true)
+				localStorage.setItem("vote_box", true);
+			else
+				localStorage.setItem("vote_box", false);
+			if($("#bots_box").is(":checked") == true)
+				localStorage.setItem("bots_box", true);
+			else
+				localStorage.setItem("bots_box", false);
+			if($("#dble_box").is(":checked") == true)
+				localStorage.setItem("dble_box", true);
+			else
+				localStorage.setItem("dble_box", false);
+		// END SAVE SET
+
+		if(getAuthorPermlink($('#link_field')[0].value) == true)
+		{
+			getInfoFull(sessionStorage.getItem("author"), sessionStorage.getItem("permlink"));
+			setTimeout(getRandomDraw, 2000);
+		}
+		else
+		{
+			$('#err_title').html("Error");
+			$('#err_body').html("Please, Provide a valid link like https://busy.org/fr/@deadzy/my-contribution");
+			$('#err_div').show();
+			$("#wait").hide();
+		}
+	});
+// EVENT END
