25cf0dfcf1308d3c1c6353bd8d13e9a4ae5efb02
deadzy <cocofaudel@gmail.com>
2018-04-23 10:58:14 +0200

    Add files via upload

--- js/randomDraw.js
+++ js/randomDraw.js
@@ -308,8 +308,7 @@ function getResult()
 		$('#part_nb').html(sessionStorage.vote_valid);
 	}
 
-	q = sessionStorage.winner;
-	list_winners = q.join(", @");
+	list_winners = win_list.join(", @");
 	$('#com_view_text').html("<a href='https://deadz.github.io/SteemRandomDraw/'><center><img src='https://deadz.github.io/SteemRandomDraw/images/random.png'/></center></a><br />"
 	+$('#sc2').text()+"<b>@"+list_winners+"</b>.");
 
